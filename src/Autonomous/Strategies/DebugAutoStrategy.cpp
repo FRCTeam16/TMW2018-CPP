22bb7d81aa3b45e4b3d72b3434a908b8f8bbf91d
Jason Smith <devjason@gmail.com>
2018-02-22 04:02:52 -0600

    Additional side auto strategies

--- src/Autonomous/Strategies/DebugAutoStrategy.cpp
+++ src/Autonomous/Strategies/DebugAutoStrategy.cpp
@@ -42,7 +42,38 @@ void DebugAutoStrategy::Init(std::shared_ptr<World> world) {
 
 DebugAutoStrategy::DebugAutoStrategy(std::shared_ptr<World> world) {
 //	SwitchScale(world);
-	ScalePickup(world);
+//	ScalePickup(world);
+	CrossField(world);
+}
+
+void DebugAutoStrategy::CrossField(std::shared_ptr<World> world) {
+	AutoStartPosition startPosition = world->GetStartPosition();
+	const bool isRight = AutoStartPosition::kRight == startPosition;
+
+	const int inv = isRight ? 1 : -1;
+	const double angle = -90.0 * inv;
+
+	steps.push_back(new ConcurrentStep({
+		new TimedDrive(angle, 0.0001, 0.0, 0.1, false),
+		new Delay(1.0),
+		new PositionElevator(Elevator::ElevatorPosition::kSwitch),
+		new PositionMast(Mast::MastPosition::kVertical),
+		new IntakeSolenoidWithDelay(false, DelayParam(DelayParam::DelayType::kNone, 0.0), 5.0)
+	}));
+
+	const double firstDriveSpeed = 0.5;
+	const double firstDriveX = 15.0;
+	const double firstDriveY = 216.0;
+
+	steps.push_back(new ClosedLoopDrive2(angle, firstDriveSpeed, firstDriveX, firstDriveY, -1, DriveUnit::Units::kInches, 8.0, 0.5, 30));
+
+	const double secondDriveSpeed = 0.3;
+	const double secondDriveX = -90.0;
+	const double secondDriveY = 0.0;
+
+	steps.push_back(new ClosedLoopDrive2(angle, secondDriveSpeed, secondDriveX, secondDriveY, -1, DriveUnit::Units::kInches, 8.0, 0.5, 30));
+
+
 }
 
 void DebugAutoStrategy::ScalePickup(std::shared_ptr<World> world) {
@@ -56,14 +87,65 @@ void DebugAutoStrategy::ScalePickup(std::shared_ptr<World> world) {
 		new TimedDrive(angle, 0.0001, 0.0, 0.1, false),
 		new Delay(1.0),
 		new PositionElevator(Elevator::ElevatorPosition::kLowScale),
-		new PositionMast(Mast::MastPosition::kVertical)
+		new PositionMast(Mast::MastPosition::kVertical),
+		new IntakeSolenoidWithDelay(false, DelayParam(DelayParam::DelayType::kNone, 0.0), 5.0)
 	}));
 
-	const double firstDriveSpeed = 0.2;
-	const double firstDriveX = 0.0;
-	const double firstDriveY = 288;
+	const double firstDriveSpeed = 0.5;
+	const double firstDriveX = 15.0;
+	const double firstDriveY = 313;
 
-	steps.push_back(new ClosedLoopDrive2(0.0, firstDriveSpeed, firstDriveX, firstDriveY, -1, DriveUnit::Units::kInches, 8.0, 0.5, 12));
+	steps.push_back(new ClosedLoopDrive2(angle, firstDriveSpeed, firstDriveX, firstDriveY, -1, DriveUnit::Units::kInches, 8.0, 0.5, 30));
+	steps.push_back(new RunIntakeWithDelay(RunIntakeWithDelay::IntakeState::Eject, DelayParam(DelayParam::DelayType::kNone, 0.0), 2.0, -1.0));
+
+	const double secondDriveSpeed = 0.3;
+	const double secondDriveX = 0.0;
+	const double secondDriveY = -79.0;
+
+	steps.push_back(new ConcurrentStep({
+		new ClosedLoopDrive2(angle, secondDriveSpeed, secondDriveX, secondDriveY, -1, DriveUnit::Units::kInches, 8.0, 0.5, 30),
+		new PositionElevator(Elevator::ElevatorPosition::kFloor, true),
+		new IntakeSolenoidWithDelay(true, DelayParam(DelayParam::DelayType::kNone, 0.0), 1.0)
+	}));
+
+
+	const double turnAngle = angle - (45 * inv);
+	steps.push_back(new Rotate(turnAngle, 10.0, 10.0, 5));
+
+	/************ DOING PICKUP OF SECOND CUBE *****/
+
+	/**
+	 * Drive until we pickup or timeout
+	 */
+	const double drive3Speed = 0.3;
+	const double drive3Angle = angle - (45 * inv);
+	const double drive3X = -48.0 * inv;
+	const double intakeDelayPos = 2 * fabs(drive3X) / 3;
+
+	ClosedLoopDrive2 *drive3 = new ClosedLoopDrive2(drive3Angle, drive3Speed, drive3X, 0.0, -1, DriveUnit::Units::kInches, 4.0, 0.5, -1);
+	drive3->SetHaltOnIntakePickup(true);
+	steps.push_back(new ConcurrentStep({
+		drive3,
+		new RunIntakeWithDelay(RunIntakeWithDelay::IntakeState::Start, DelayParam(DelayParam::DelayType::kNone, 0.0), 0.1, 0.0),
+		new IntakeSolenoidWithDelay(false, DelayParam(DelayParam::DelayType::kPosition, intakeDelayPos), 5.0)
+	}, true));
+
+
+
+	const double drive4Angle = -70;	// was drive3angle
+	const double drive4X = 12 * inv;
+	const double drive4Y = 90.0;
+	ClosedLoopDrive2 *drive4 = new ClosedLoopDrive2(drive4Angle, drive3Speed, drive4X, drive4Y, -1, DriveUnit::Units::kInches, 5.0, 0.5, 12);
+	drive4->UsePickupDistance();
+	steps.push_back(new ConcurrentStep({
+		drive4,
+		new RunIntakeWithDelay(RunIntakeWithDelay::IntakeState::Stop, DelayParam(DelayParam::DelayType::kNone, 0.0), 0.1, -1),
+		new PositionElevator(Elevator::ElevatorPosition::kHighScale, DelayParam(DelayParam::DelayType::kTime, 0.5), true)
+	}, true));
+
+
+	steps.push_back(new RunIntakeWithDelay(RunIntakeWithDelay::IntakeState::Eject, DelayParam(DelayParam::DelayType::kNone, 0.0), 2.0, -1.0));
+
 }
 
 
@@ -113,6 +195,8 @@ void DebugAutoStrategy::SwitchScale(std::shared_ptr<World> world) {
 	}, true));
 
 
+	/************ DOING PICKUP OF SECOND CUBE *****/
+
 	/**
 	 * Drive until we pickup or timeout
 	 */
@@ -144,53 +228,5 @@ void DebugAutoStrategy::SwitchScale(std::shared_ptr<World> world) {
 
 	steps.push_back(new RunIntakeWithDelay(RunIntakeWithDelay::IntakeState::Eject, DelayParam(DelayParam::DelayType::kNone, 0.0), 2.0, -1.0));
 }
-
-/**
- *
-	const int inv = isRight ? 1 : -1;
-	const double firstAngleTarget = -15.0 * inv;
-	const double firstAngleThreshold = -90.0 * inv;
-
-	const double autoScalePickupSpeed = 0.3;
-	const double autoScalePickupY = 48;
-	const double autoScalePickupX = 14 * inv;
-
-
-	steps.push_back(new ConcurrentStep({
-		new TimedDrive(-180.0, 0.2, 0.0, 0.2),
-		new PositionElevator(Elevator::ElevatorPosition::kSwitch)
-	}));
-	steps.push_back(
-			new ConcurrentStep({
-				new Rotate(firstAngleTarget, 5.0, 5.0, 10),
-				new PositionElevator(Elevator::ElevatorPosition::kHighScale, true),
-	}));
-	steps.push_back(
-		new ClosedLoopDrive2(firstAngleTarget, autoScalePickupSpeed, autoScalePickupX, autoScalePickupY, -1, DriveUnit::Units::kInches, 5.0, 0.5, 24)
-	);
-	steps.push_back(new EjectCube(0.0, 5.0, 1.0));
- */
-
-/**
- * steps.push_back(new PositionElevator(Elevator::ElevatorPosition::kHighScale, true));
-	steps.push_back(new Delay(2.0));
-
-	steps.push_back(new RotateUntilPast(isRight, 180.0, 90.0));
-	steps.push_back(
-			new ConcurrentStep({
-				new Rotate(180.0, 5.0, 5.0, 10),
-		        new PositionElevator(Elevator::ElevatorPosition::kFloor, true),
-				new IntakeSolenoid(true)
-	}));
-
-	steps.push_back(
-				new ConcurrentStep({
-					new ClosedLoopDrive2(180.0, 0.2, -6, -12, -1, DriveUnit::Units::kInches, 3.0, -1, -1),
-					new IntakeCube(3.0, 3.5, 4.0)
-	}));
-
-	steps.push_back(new StopStep());
- */
-
 
 
--- src/Autonomous/Strategies/DebugAutoStrategy.h
+++ src/Autonomous/Strategies/DebugAutoStrategy.h
@@ -25,6 +25,7 @@ private:
 
 	void SwitchScale(std::shared_ptr<World> world);
 	void ScalePickup(std::shared_ptr<World> world);
+	void CrossField(std::shared_ptr<World> world);
 };
 
 #endif /* SRC_AUTONOMOUS_DEBUGAUTOSTRATEGY_H_ */
