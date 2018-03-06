3fb218d71f0276ed731c9b1e3dbad839ab5b3e66
Jason Smith <devjason@gmail.com>
2018-03-05 21:25:52 -0600

    Adding initial subsystem, previously had link errors

--- src/Subsystems/StatusReporter.h
+++ src/Subsystems/StatusReporter.h
@@ -1,27 +1,24 @@
 /*
- * LEDCommunications.h
+ * StatusReporter.h
  *
- *  Created on: Mar 2, 2017
- *      Author: User
+ *  Created on: Mar 5, 2018
+ *      Author: jsmith
  */
 
 #ifndef SRC_SUBSYSTEMS_STATUSREPORTER_H_
 #define SRC_SUBSYSTEMS_STATUSREPORTER_H_
 
 #include "WPILib.h"
-#include "Robot.h"
-#include "Robot.cpp"
 
 class StatusReporter {
-private:
-	std::unique_ptr<I2C> i2c;
-	int counter = 0;
-	const int scanFrequency = 1;
-	void SendData();
 public:
-	StatusReporter(int portNumber = 16);
+	StatusReporter(int deviceAddress = 16);
 	virtual ~StatusReporter() {}
 	void Run();
+private:
+	std::unique_ptr<I2C> i2c;
+
+	void SendData();
 };
 
 #endif /* SRC_SUBSYSTEMS_STATUSREPORTER_H_ */
